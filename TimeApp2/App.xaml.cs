f04835ce3bed377da5fa6d04f07f329c6a254c0e
benrwb <benrwb@googlemail.com>
2013-09-03 17:33:51 +0100

    follow cursor

--- TimeApp2/App.xaml.cs
+++ TimeApp2/App.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +16,27 @@ namespace TimeApp2
     /// </summary>
     public partial class App : Application
     {
+        public static void Log(string msg)
+        {
+            using (var sr = new StreamWriter(@"c:\users\user\log.txt", true))
+            {
+                sr.WriteLine(DateTime.Now + "\t" + Assembly.GetEntryAssembly().Location + "\t" + msg);
+            }
+        }
+
+
+
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Log("App started");
+        }
+
+       
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            Log("App exit");
+        }
     }
 }
--- TimeApp2/MainWindow.xaml.cs
+++ TimeApp2/MainWindow.xaml.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -21,32 +23,34 @@ namespace TimeApp2
     /// </summary>
     public partial class MainWindow : Window
     {
-        System.Threading.EventWaitHandle ewh; // support for running on multiple desktops 
+        System.Threading.EventWaitHandle ewh; // support for running on multiple desktops
+
 
         public MainWindow()
         {
             ewh = new System.Threading.EventWaitHandle(false, System.Threading.EventResetMode.AutoReset, @"Global\TIMEAPP-" + Desktops.GetProcessDesktopName());
 
             InitializeComponent();
-            this.Hide();
 
-            Timer t = new Timer();
-            t.Interval = 60000;
+
+
+
+            Timer t = new Timer() { Interval = 60000 };
             t.Elapsed += t_Elapsed;
             t.Start();
 
-            Timer t2 = new Timer();
-            t2.Interval = 1000;
+            Timer t2 = new Timer() { Interval = 1000, AutoReset = false };
             t2.Elapsed += t_Elapsed;
-
-
-            t2.AutoReset = false;
             t2.Start();
-
         }
 
         void t_Elapsed(object sender, ElapsedEventArgs e)
         {
+            DateTime now = DateTime.Now;
+            if (now.Hour < 5) now = now.AddDays(-1);
+            // e.g. late Saturday night is technically early Sunday morning, 
+            //      but we want to class it as Saturday instead.
+
             int level = 0;
             if (new[] { DayOfWeek.Sunday, DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday }.Contains(DateTime.Now.DayOfWeek))
             {
@@ -83,6 +87,8 @@ namespace TimeApp2
 
 
 
+            new System.Threading.Thread(pos).Start(); // follow the mouse cursor for 1.2 seconds
+            System.Threading.Thread.Sleep(100);// allow time for thread to start
 
             Dispatcher.Invoke((Action)delegate
             {
@@ -91,12 +97,13 @@ namespace TimeApp2
             });
 
             Brush color = (level == 2 ? Brushes.Red : Brushes.LimeGreen);
+            Brush altcolor = (level == 2 ? Brushes.Black : Brushes.White);
             for (int i = 0; i < 5; i++)
             {
                 Dispatcher.Invoke((Action)delegate
                 {
-                    label1.Foreground = (i % 2 == 0) ? color : Brushes.Black;
-                    this.Background = (i % 2 != 0) ? color : Brushes.Black;
+                    label1.Foreground = (i % 2 == 0) ? color : altcolor;
+                    border1.Background = (i % 2 != 0) ? color : altcolor;
                 });
                 System.Threading.Thread.Sleep(250);
             }
@@ -108,9 +115,67 @@ namespace TimeApp2
             });
         }
 
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+
+
+
+        void pos()
         {
-            ewh.Close();
+
+            IntPtr hwnd = IntPtr.Zero;
+            while (hwnd == IntPtr.Zero)
+            {
+                Dispatcher.Invoke((Action)delegate
+                {
+                    hwnd = new WindowInteropHelper(this).Handle;
+                });
+                if (hwnd == IntPtr.Zero) { System.Threading.Thread.Sleep(100); } // wait for window handle to become available
+            }
+
+            int ms = 15;  // 15ms ~ 60fps 
+            // ms to fps
+            // 1000 = 1
+            // 500  = 2
+            // 250  = 4
+            // 125  = 8
+            // 62.5 = 16
+            // 31.25 = 32
+            // 15.625 = 64
+
+            for (int i = 0; i < 1200; i += ms) // follow the mouse cursor for 1.2 seconds, then exit
+            {
+                var pos = GetMousePosition();
+
+                SetWindowPos(hwnd, IntPtr.Zero, (int)pos.X + 10, (int)pos.Y + 10, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+
+                System.Threading.Thread.Sleep(ms);
+
+            }
         }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        const int SWP_NOZORDER = 0x0004;
+        const int SWP_NOSIZE = 0x0001;
+
+
+
     }
 }
